180e80a933f576c4890b708de2dfaa295731cda2
SuHyeon-CHOI <tngus4869@gmail.com>
2023-11-27 17:04:58 +0900

    231127

--- Chapter2/src/ch16/Employee.java
+++ Chapter2/src/ch16/Employee.java
@@ -2,7 +2,7 @@ package ch16;
 
 public class Employee {
 	
-	public static int serialNum = 1000;
+	private static int serialNum = 1000;
 	
 	private int employeeId;
 	private String employeeName;
@@ -14,6 +14,13 @@ public class Employee {
 	}
 	
 	
+	
+	public static int getSerialNum() {
+		return serialNum;
+	}
+
+
+
 	public int getEmployeeId() {
 		return employeeId;
 	}
